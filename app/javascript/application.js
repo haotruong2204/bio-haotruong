7a2392927ef7d3ff42a4b3021cf12e73c3d4ea1f
Hao Truong <haotv03@gmail.com>
2023-04-06 15:18:45 +0700

    config editor

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,10 +2,44 @@
 import "@hotwired/turbo-rails";
 import { Turbo } from "@hotwired/turbo-rails";
 import "controllers";
-// import "jquery";
-// import "lodash";
-// import "select2";
-// import "flatpickr";
+import "jquery";
+import "lodash";
+import "select2";
+import "flatpickr";
 import "custom/toast";
 
 Turbo.session.drive = false;
+
+const init_select2_tag = (selector) => {
+  $(selector).select2({
+    tags: true
+  });
+};
+
+const init_select2 = (selector) => {
+  $(selector).select2();
+};
+
+init_select2('.select2');
+init_select2_tag('.select2-tag');
+
+$(".flatpick").flatpickr(
+  {
+    enableTime: true,
+    dateFormat: "Y/m/d H:i",
+  }
+);
+
+$('input[name="post[status]"]').on("change", function(){
+  if($(this).val() == "2") {
+    $("#published_time").addClass("d-none");
+    $(".clear-time").addClass("d-none");
+    $(".set-time").removeClass("d-none");
+    $("#published_at").val("");
+    $(".info-visible").remove();
+    $(".visible-time").remove();
+    $(".set-time").addClass("d-none");
+  } else {
+    $(".set-time").removeClass("d-none");
+  }
+})
